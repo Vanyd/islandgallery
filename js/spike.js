551e1da7f52814bcca967cb0ab378ef4e347f0c3
Colum Coughlan <ccou0002@iec02ph458fvh8.home>
2016-11-22 01:18:18 +0000

    first commit

---
+++ js/spike.js
@@ -0,0 +1,113 @@
+
+function colinMoreInfo(x) {
+    switch(x){
+        case 1:
+            document.getElementById("moreInformationPicture").src = "images/paintings/colincarruthers/Grasses-West-Sands.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Grasses West Sands</b><br /><br />Oil on Canvas<br /><br>Guide Price: €1,650";
+            break;
+        case 2:
+            document.getElementById("moreInformationPicture").src = "images/paintings/colincarruthers/Sanna-Bay.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Sanna Bay</b><br /><br />Oil on Canvas<br /><br>Guide Price: €1,950";
+            break;
+        case 3:
+            document.getElementById("moreInformationPicture").src = "images/paintings/colincarruthers/Shore-Poppies.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Shore Poppies</b><br /><br />Oil on Canvas<br /><br>Guide Price: €1,750";
+            break;
+        case 4:
+            document.getElementById("moreInformationPicture").src = "images/paintings/colincarruthers/Sunlit-Grasses.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Sunlit Grasses</b><br /><br />Oil on Canvas<br /><br>Guide Price: €1,750";
+            break;
+        case 5:
+            document.getElementById("moreInformationPicture").src = "images/paintings/colincarruthers/Light-Breaks.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Light Breaks</b><br /><br />Oil on Canvas<br /><br>Guide Price: €1,350";
+            break;
+        case 6:
+            document.getElementById("moreInformationPicture").src = "images/paintings/colincarruthers/Still-Water-II.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Still Water II</b><br /><br />Oil on Canvas<br /><br>Guide Price: €1,450";
+            break;
+        case 7:
+            document.getElementById("moreInformationPicture").src = "images/paintings/colincarruthers/Sunlit-River-Bank.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Sunlit River Bank</b><br /><br />Oil on Canvas<br /><br>Guide Price: €1,850";
+            break;
+        case 8:
+            document.getElementById("moreInformationPicture").src = "images/paintings/colincarruthers/Upturned-Canoe-and-Boats-Iona.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Upturned Canoe & Boats Iona</b><br /><br />Oil on Canvas<br /><br>Guide Price: €1,650";
+            break;
+    }
+}
+
+
+function carolMoreInfo(x) {
+    switch(x){
+        case 1:
+            document.getElementById("moreInformationPicture").src = "images/paintings/carolanncarrigan/Crashing-Wave.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Crashing Wave</b><br /><br />Oil on Canvas<br /><br>Guide Price: €1,650";
+            break;
+        case 2:
+            document.getElementById("moreInformationPicture").src = "images/paintings/carolanncarrigan/Evening-Stroll.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Evening Stroll</b><br /><br />Oil on Canvas<br /><br>Guide Price: €1,950";
+            break;
+        case 3:
+            document.getElementById("moreInformationPicture").src = "images/paintings/carolanncarrigan/Rock-n-Wave.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Rock n Wave</b><br /><br />Oil on Canvas<br /><br>Guide Price: €1,750";
+            break;
+        case 4:
+            document.getElementById("moreInformationPicture").src = "images/paintings/carolanncarrigan/Rolling-in-at-Sandycove.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Rolling in at Sandycove</b><br /><br />Oil on Canvas<br /><br>Guide Price: €1,750";
+            break;
+        case 5:
+            document.getElementById("moreInformationPicture").src = "images/paintings/carolanncarrigan/Sandycove-Spray.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Sandycove Spray</b><br /><br />Oil on Canvas<br /><br>Guide Price: €1,350";
+            break;
+        case 6:
+            document.getElementById("moreInformationPicture").src = "images/paintings/carolanncarrigan/Sea-Splash.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Sea Splash</b><br /><br />Oil on Canvas<br /><br>Guide Price: €1,450";
+            break;
+        case 7:
+            document.getElementById("moreInformationPicture").src = "images/paintings/carolanncarrigan/Seagull-Saunter.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Seagull Saunter</b><br /><br />Oil on Canvas<br /><br>Guide Price: €1,850";
+            break;
+        case 8:
+            document.getElementById("moreInformationPicture").src = "images/paintings/carolanncarrigan/Still-Three.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Still Three</b><br /><br />Oil on Canvas<br /><br>Guide Price: €1,650";
+            break;
+    }
+}
+
+
+function johnMoreInfo(x) {
+    switch(x){
+        case 1:
+            document.getElementById("moreInformationPicture").src = "images/paintings/johnogrady/Evening-West-Of-Ireland.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Evening West Of Ireland</b><br /><br />Oil on Panel<br /><br>Guide Price: €880";
+            break;
+        case 2:
+            document.getElementById("moreInformationPicture").src = "images/paintings/johnogrady/Flurry-On-The-Dublin-Mountains.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Flurry On The Dublin Mountains</b><br /><br />Oil on Panel<br /><br>Guide Price: €890";
+            break;
+        case 3:
+            document.getElementById("moreInformationPicture").src = "images/paintings/johnogrady/You-Are-Everything-II.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>You Are Everything II</b><br /><br />Oil on Panel<br /><br>Guide Price: €975";
+            break;
+        case 4:
+            document.getElementById("moreInformationPicture").src = "images/paintings/johnogrady/Purple-And-Maroon-Heather.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Bog Heather</b><br /><br />Oil on Panel<br /><br>Guide Price: €825";
+            break;
+        case 5:
+            document.getElementById("moreInformationPicture").src = "images/paintings/johnogrady/The-Southwesterly.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>The Southwesterly</b><br /><br />Oil on Panel<br /><br>Guide Price: €950";
+            break;
+        case 6:
+            document.getElementById("moreInformationPicture").src = "images/paintings/johnogrady/The-Turning-Of-The-Year.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>The Turning Of The Year</b><br /><br />Oil on Panel<br /><br>Guide Price: €930";
+            break;
+        case 7:
+            document.getElementById("moreInformationPicture").src = "images/paintings/johnogrady/Valley-of-Lights.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Valley Of Lights</b><br /><br />Oil on Panel<br /><br>Guide Price: €790";
+            break;
+        case 8:
+            document.getElementById("moreInformationPicture").src = "images/paintings/johnogrady/winter-on-mount-kippure.jpg";
+            document.getElementById("moreInformationText").innerHTML = "<b>Winter On Mount Kippure</b><br /><br />Oil on Panel<br /><br>Guide Price: €925";
+            break;
+    }
+}
